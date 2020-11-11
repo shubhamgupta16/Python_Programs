7b6d062b63f16c56560f0c13ee01bd8ec4c39b08
shubhamgupta16 <guptashubham7777@gmail.com>
2020-11-11 15:04:40 +0530

    Move to Folder

--- chapter 2/13_chap2_exer1.py
+++ chapter 2/13_chap2_exer1.py
@@ -1,3 +1,7 @@
+#Ask user to input 3 numbers and you have to print average of three numbers using string formatting.
+
+#Bonus :- try to take all three comma separated inputs in one line.
+
 # find average of three numbers
 number1, number2, number3 = input("Enter three numbers seprated by comma: ").split(",") 
 print(f"Average of {number1}, {number2} and {number3} is {(int(number1) + int(number2) + int(number3))/3}")
---
+++ chapter 2/16_step_argument_slicing.py
@@ -0,0 +1,12 @@
+# slicing / selecting sub sequences
+lang = "Python"
+# syntax - [start argument : stop argument - 1 : step arguments]
+# print(lang[0:6:2])
+print("Shubham"[0::2]) #output: Suhm
+# print("Shubham"[0:6:2])  #output: Suh
+# print("Shubham"[:6:2])  #output: Suh
+
+# for do reverse string
+print("Shubham"[6::-1])  #output: mahbuhS
+print("Shubham"[-1::-1])  #output: mahbuhS
+print("Shubham"[::-1])  #output: mahbuhS
